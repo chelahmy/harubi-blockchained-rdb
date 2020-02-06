b710a44046477942c2dd4b516ceefb90815c9f5e
Abdullah Daud <chelahmy@gmail.com>
2020-02-06 22:27:54 +0800

    Refactored backend fetching for signup

--- src/js/signup.jsx
+++ src/js/signup.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import BackendRequest from './serve'
 import Form from './form'
 import Input from './input'
 
 export default class SignUp extends React.Component {
   constructor(props) {
     super(props)
-
     this.handleOnFormMount = this.handleOnFormMount.bind(this)
     this.handleOnFormSubmit = this.handleOnFormSubmit.bind(this)
   }
@@ -17,41 +17,21 @@ export default class SignUp extends React.Component {
   handleOnFormSubmit(ev,frm) {
     // Get data from the form
     let data = new FormData(ev.target)
-    let request = {
+    BackendRequest({ // params
       model: 'user',
       action: 'signup',
       name: data.get('name'),
       password: data.get('password'),
       email: data.get('email')
-    }
-    console.log($.param(request))
-    // Post the data to the server
-    fetch('/backend/serve.php', {
-      method: 'POST',
-      body: new URLSearchParams($.param(request))
-    })
-    // On server response
-    .then((respond) => {
-      if (respond.ok) { // See the fetch() documentation for details
-        return respond.json() // pass to the next .then as resp_json
-      }
-      else
-        this.form.callOut('Server Error', 'Could not process the request', 'alert')
-    })
-    // On server respond.ok (see above)
-    .then((resp_json) => {
-      // Application implemented response
-      if (resp_json.status != 0) {
-        //this.form.callOut('Server Response', resp_json.results, 'success')
-        this.props.page.navigate(this.props.onSuccessPage)
-      }
-      else {
-        this.form.callOut('Server Response', resp_json.error_message, 'alert')
-      }
-    })
-    // On client or network error
-    .catch((error) => {
-      this.form.callOut('System Error', 'Could not deliver the request: ' + error, 'alert')
+    },
+    (resp) => { // success
+      this.props.page.navigate(this.props.onSuccessPage)
+    },
+    (title, message) => { // error
+      this.form.callOut(title, message, 'alert')
+    },
+    () => { // reset
+      this.props.page.navigate('home')
     })
   }
 
